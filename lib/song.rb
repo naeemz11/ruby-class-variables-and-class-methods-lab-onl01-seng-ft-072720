89886bdfb46b6133089499f28c1dd94d232781e5
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-26 15:36:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,43 +1,48 @@
-class Song
+require 'pry'
+class Song 
+  
   attr_accessor :name, :artist, :genre
-
-  @@count = 0
-  @@genres = []
-  @@artists = []
-
-  def initialize(song_name, artist, genre)
-    @name = song_name
-    @artist = artist
-    @genre = genre
-    @@count += 1
-    @@genres << genre
+  
+  @@count= 0 
+  @@artists=[]
+  @@genres= []
+  
+  
+  def initialize(name, artist, genre)
+    @name= name 
+    @artist= artist
+    @genre= genre
+    @@count += 1 
     @@artists << artist
-  end
-
-  def self.count
+    @@genres << genre 
+  end 
+  
+  def self.count 
     @@count
-  end
-
-  def self.genres
-    @@genres.uniq!
-  end
-
+  end 
+  
   def self.artists
-    @@artists.uniq!
+    @@artists.uniq
+    #binding.pry
   end
-
+  
+  def self.genres
+    @@genres.uniq
+  end 
+  
   def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre] 
-        genre_count[genre] += 1
-      else #if it's a new genre
-        genre_count[genre] = 1
-      end
-    end
+  genre_count= {}
+  @@genres.each do |genre|
+    if genre_count[genre]
+      genre_count[genre] +=1 
+    else 
+      genre_count[genre] = 1 
+    end 
+    
+  end 
     genre_count
-  end
-
+  end 
+    
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
@@ -46,7 +51,117 @@ class Song
       else
         artist_count[artist] = 1
       end
+   
     end
     artist_count
   end
 end
+  
+  lucifer= Song.new("Lucifer", "Jay-Z", "rap" ) 
+  ninety_nine_problems=Song.new("99 Problems", "Jay-Z", "rap") 
+  hit_me= Song.new("hit me baby one more time", "Brittany Spears", "pop") 
+  
+  puts lucifer.artist
+  binding.pry 
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Song
+#   attr_accessor :name, :artist, :genre
+
+#   @@count = 0
+#   @@genres = []
+#   @@artists = []
+
+#   def initialize(song_name, artist, genre)
+#     @name = song_name
+#     @artist = artist
+#     @genre = genre
+#     @@count += 1
+#     @@genres << genre
+#     @@artists << artist
+#   end
+
+#   def self.count
+#     @@count
+#   end
+
+#   def self.genres
+#     @@genres.uniq!
+#   end
+
+#   def self.artists
+#     @@artists.uniq!
+#   end
+
+#   def self.genre_count
+#     genre_count = {}
+#     @@genres.each do |genre|
+#       if genre_count[genre] 
+#         genre_count[genre] += 1
+#       else #if it's a new genre
+#         genre_count[genre] = 1
+#       end
+#     end
+#     genre_count
+#   end
+
+#   def self.artist_count
+#     artist_count = {}
+#     @@artists.each do |artist|
+#       if artist_count[artist] 
+#         artist_count[artist] += 1
+#       else
+#         artist_count[artist] = 1
+#       end
+#     end
+#     artist_count
+#   end
+# end
